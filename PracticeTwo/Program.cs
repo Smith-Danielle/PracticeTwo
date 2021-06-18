95f2c274a2812c30c23f1e2c3472a3d89bd0faf6
Danielle Smith <smith.ddms@gmail.com>
2021-06-17 21:04:07 -0500

    6-17-21 Practice

--- PracticeTwo/Program.cs
+++ PracticeTwo/Program.cs
@@ -44,62 +44,62 @@ namespace PracticeTwo
         //{
 
 
-            /*Practicing Selection Statements if else if switch
-            Console.WriteLine("Try to guess my favorite number.");
-            Console.WriteLine("Enter your first guess to begin.");
+        /*Practicing Selection Statements if else if switch
+        Console.WriteLine("Try to guess my favorite number.");
+        Console.WriteLine("Enter your first guess to begin.");
 
-            string guess = Console.ReadLine();
-            var myNum = 8;
-            string statement = "help";
+        string guess = Console.ReadLine();
+        var myNum = 8;
+        string statement = "help";
 
-            do
+        do
+        {
+            if (guess == statement)
             {
-                if (guess == statement)
-                {
-                    Console.WriteLine("Lower than 50, but higher than 5.");
-                }
-                else if (Convert.ToInt32(guess) >= 1000)
-                {
-                    Console.WriteLine("Way too high!!");
-                    Console.WriteLine("Consider typing 'help' to get a hint.");
-                }
-                else if (Convert.ToInt32(guess) >= 13)
-                {
-                    Console.WriteLine("Try reducing your number!");
-                }
-                else if (Convert.ToInt32(guess) <= 12 && Convert.ToInt32(guess) >= 9)
-                {
-                    Console.WriteLine("You are really close! A little lower.");
-                }
-                else if (Convert.ToInt32(guess) <= 7 && Convert.ToInt32(guess) >= 4)
-                {
-                    Console.WriteLine("You are really close! A little higher.");
-                }
-                else if (Convert.ToInt32(guess) <= 3)
-                {
-                    Console.WriteLine("Too low!");
-                }
-                else
-                {
+                Console.WriteLine("Lower than 50, but higher than 5.");
+            }
+            else if (Convert.ToInt32(guess) >= 1000)
+            {
+                Console.WriteLine("Way too high!!");
+                Console.WriteLine("Consider typing 'help' to get a hint.");
+            }
+            else if (Convert.ToInt32(guess) >= 13)
+            {
+                Console.WriteLine("Try reducing your number!");
+            }
+            else if (Convert.ToInt32(guess) <= 12 && Convert.ToInt32(guess) >= 9)
+            {
+                Console.WriteLine("You are really close! A little lower.");
+            }
+            else if (Convert.ToInt32(guess) <= 7 && Convert.ToInt32(guess) >= 4)
+            {
+                Console.WriteLine("You are really close! A little higher.");
+            }
+            else if (Convert.ToInt32(guess) <= 3)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else
+            {
+                Console.WriteLine("Excellent! You guessed my favorite number!!");
+                break;
+            }
+
+            Console.WriteLine("Enter your next guess:");
+            guess = Console.ReadLine();
+
+           switch (guess)
+            {
+                case "help" :
+                    break;
+                case "8":
                     Console.WriteLine("Excellent! You guessed my favorite number!!");
                     break;
-                }
-
-                Console.WriteLine("Enter your next guess:");
-                guess = Console.ReadLine();
-
-               switch (guess)
-                {
-                    case "help" :
-                        break;
-                    case "8":
-                        Console.WriteLine("Excellent! You guessed my favorite number!!");
-                        break;
-                }
-
-
             }
-            while (guess == statement || Convert.ToInt32(guess) != myNum);*/
+
+
+        }
+        while (guess == statement || Convert.ToInt32(guess) != myNum);*/
 
         /* array assignment
          int[] num = new int[5];
@@ -211,6 +211,55 @@ namespace PracticeTwo
                 return false;
             }
         }*/
+        /*static void Main(string[] args)
+        {
+            
+        }
+        public static class SpacesRemover
+        {
+            public static string NoSpace(string input)
+            {
+                input = input.Replace(" ", "");
+                return input;
+            }
+        }*/
+        /* CodeWars
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RepeatStr(3, "Danielle"));
+        }
+        public static string RepeatStr(int n, string s)
+        {
+            string newString = "";
+            for (int i = 0; i < n; i++)
+            {
+                newString += s;
+            }
+            return newString;
+        }*/
+        /* SoloLearn Lesson 23
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            int[] threes = new int[number];
+            for (int i = 0; i < threes.Length; i++)
+            {
+                threes[i] = i + 1;
+            }
+            foreach (int num in threes)
+            {
+                if (num % 3 == 0)
+                {
+                    Console.WriteLine("*");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }*/
+
     }
 }
 
